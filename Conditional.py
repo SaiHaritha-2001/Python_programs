6db9305bc181a5025f40572a9d5bdc057efcf186
SaiHaritha-2001 <hkolluru27@gmail.com>
2025-09-11 16:54:09 +0530

    Initial commit with Python files and README

---
+++ Conditional.py
@@ -0,0 +1,10 @@
+#if(condition)
+Weather=input("Enter the weather: ")
+if(Weather=="Sunny"):
+    print("Go and play Cricket")
+elif(Weather=="Rainy"):
+    print("I will play with cars")
+else:
+    print("I will play with robots")
+print("Have fun My dear")    
+
